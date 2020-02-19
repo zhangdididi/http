b20b67dc44723932a5eb5acb9ae2c2ed99dfb749
zhangdididi <49708573+zhangdididi@users.noreply.github.com>
2020-02-19 15:03:55 +0800

    Update HttpTask.java

--- src/http/HttpTask.java
+++ src/http/HttpTask.java
@@ -26,7 +26,6 @@ public class HttpTask implements Runnable {
 
     @Override
     public void run() {
-
         try {
             //1. 根据解析出的request对象中属性，来进行逻辑处理
             //2. 在不同逻辑中将要返回的数据设置到response对象中
@@ -35,17 +34,29 @@ public class HttpTask implements Runnable {
             //调整业务逻辑
             //1. 根据url在WebApp文件夹下去找是否存在资源，存在就返回资源
 
-            response.setStatusNum(200);
-            response.setMessage("ok");
             //改造：读取项目中的Login.html文件内容，并返回给客户端
-            //1. html文件所在的WebApp需要设置为resource资源文件夹
+            //相对路径读取
+            //(1). html文件所在的WebApp需要设置为resource资源文件夹
             // -->将WebApp中所有文件复制到编译后的输出文件夹
-            //2. 通过getClassLoader().getResourceAsStream()获取文件的输入流
-            InputStream is = this.getClass().getClassLoader().getResourceAsStream("login.html");
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            String content;
-            while ((content = br.readLine()) != null) {
-                response.println(content);
+            //(2). 通过getClassLoader().getResourceAsStream()获取文件的输入流
+            InputStream is = this.getClass().getClassLoader().
+                    getResourceAsStream("." + request.getUrl());//http url中
+            //存在就返回资源
+            if (is != null) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                String content;
+                while ((content = br.readLine()) != null) {
+                    response.println(content);
+                }
+                response.setStatusNum(200);
+                response.setMessage("ok");
+            } else if ("/login".equals(request.getUrl())) {
+
+            } else {
+                //以上路径找不到，说明服务器不提供这个url服务，返回404
+                response.setStatusNum(404);
+                response.setMessage("Not Found");
+                System.out.println("找不到资源");
             }
             response.flush();
         } catch (IOException e) {
